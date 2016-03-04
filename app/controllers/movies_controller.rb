3e97cc7ce26ac3b0aa79b22f4cb0ccf57b429836
Ghanshyam Bhutra <mail.gbhutra@gmail.com>
2016-03-03 20:54:29 -0600

    hw 2 part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,5 @@
 class MoviesController < ApplicationController
 
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
-
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -11,18 +7,21 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    #@movies = Movie.all
+    movie = Movie
+    @q = movie.search(params[:q])
+    @movies = @q.result(:distinct => true)
   end
 
   def new
     # default: render 'new' template
   end
 
-  def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
-  end
+  #def create
+    #@movie = Movie.create!(movie_params)
+    #flash[:notice] = "#{@movie.title} was successfully created."
+    #redirect_to movies_path
+  #end
 
   def edit
     @movie = Movie.find params[:id]
@@ -43,3 +42,5 @@ class MoviesController < ApplicationController
   end
 
 end
+
+
